b9c558ce21cbb0986513f07bce33f2ab2ead0103
Gabriel Chartier <gabriel@chartier.link>
2022-05-17 21:39:35 -0400

    Wire up image flower to be movable

--- components/image-garden/ImageFlower.tsx
+++ components/image-garden/ImageFlower.tsx
@@ -1,19 +1,30 @@
-import { ImageFlowerStem } from "@components/image-garden/ImageFlowerStem";
 import Image from "next/image";
 import styled from "styled-components";
+import { ImageFlowerStem as Stem } from "@components/image-garden/ImageFlowerStem";
 
 interface Props {
     src: string;
     alt: string;
     width?: number;
     height?: number;
+    xPosition?: string;
+    yPosition?: string;
 }
 
-export function ImageFlower({ src, alt, width = 30, height = 30 }: Props) {
+export function ImageFlower({
+    src,
+    alt,
+    width = 30,
+    height = 30,
+    xPosition = "0px",
+    yPosition = "0px",
+}: Props) {
     return (
         <ImageFlowerWrapper>
-            <Image src={src} alt={alt} width={width} height={height} />
-            <ImageFlowerStem />
+            <Flower yPosition={yPosition} xPosition={xPosition}>
+                <Image src={src} alt={alt} width={width} height={height} />
+            </Flower>
+            <Stem />
         </ImageFlowerWrapper>
     );
 }
@@ -24,4 +35,14 @@ const ImageFlowerWrapper = styled.div`
     justify-content: center;
     align-items: center;
     position: absolute;
+`;
+
+interface FlowerProps {
+    xPosition: string;
+    yPosition: string;
+}
+
+const Flower = styled.div<FlowerProps>`
+    margin-bottom: ${(props) => props.yPosition};
+    margin-left: ${(props) => props.xPosition};
 `;
