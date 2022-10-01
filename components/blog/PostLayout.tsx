67f6eb71d0552fa6b2e7cece132b7bb70cd4928b
Gabriel Chartier <gabriel@chartier.link>
2022-09-30 22:28:41 -0400

    WIP

--- components/blog/PostLayout.tsx
+++ components/blog/PostLayout.tsx
@@ -1,43 +1,10 @@
+import Link from "next/link";
+
 export default function PostLayout({ children }: { children: any }) {
     return (
-        <>
-            <div className="wrapper">{children}</div>
-            <style jsx>{`
-                .wrapper {
-                    max-width: 36rem;
-                    margin: 0 auto;
-                    padding: 1.5rem;
-                }
-            `}</style>
-            <style jsx global>{`
-                * {
-                    margin: 0;
-                    padding: 0;
-                }
-                :root {
-                    --site-color: royalblue;
-                    --divider-color: rgba(0, 0, 0, 0.4);
-                }
-                html {
-                    font: 100%/1.5 system-ui;
-                }
-                a {
-                    color: inherit;
-                    text-decoration-color: var(--divider-color);
-                    text-decoration-thickness: 2px;
-                }
-                a:hover {
-                    color: var(--site-color);
-                    text-decoration-color: currentcolor;
-                }
-                h1,
-                p {
-                    margin-bottom: 1.5rem;
-                }
-                code {
-                    font-family: "Menlo";
-                }
-            `}</style>
-        </>
-    )
+        <div className="max-w-xl mx-0 my-auto p-6">
+            <div className="bg-green-600 h-3 w-3 p-2">test</div>
+            {children}
+        </div>
+    );
 }
--- pages/blog/posts/[slug].tsx
+++ pages/blog/posts/[slug].tsx
@@ -1,102 +1,68 @@
-import fs from "fs"
-import matter from "gray-matter"
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote"
-import { serialize } from "next-mdx-remote/serialize"
-import dynamic from "next/dynamic"
-import Head from "next/head"
-import Link from "next/link"
-import path from "path"
-import { postFilePaths, POSTS_PATH } from "@util/mdxUtils"
-import { SpicyLink } from "@components/blog/SpicyLink"
-import { GifDivider } from "@components/blog/GifDivider"
-import PostLayout from "@components/blog/PostLayout"
-import { PostFrontmatter } from "types/Post"
+import fs from "fs";
+import matter from "gray-matter";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Head from "next/head";
+import path from "path";
+import { postFilePaths, POSTS_PATH } from "@util/mdxUtils";
+import { SpicyLink } from "@components/blog/SpicyLink";
+import { GifDivider } from "@components/blog/GifDivider";
+import PostLayout from "@components/blog/PostLayout";
+import { PostFrontmatter } from "types/Post";
 
-// Custom components/renderers to pass to MDX.
-// Since the MDX files aren't loaded by webpack, they have no knowledge of how
-// to handle import statements. Instead, you must include components in scope
-// here.
-const defaultComponents = { SpicyLink, GifDivider }
-const components = {
-    ...defaultComponents,
-    // a: SpicyLink,
-    // It also works with dynamically-imported components, which is especially
-    // useful for conditionally loading components for certain routes.
-    // See the notes in README.md for more details.
-    //SpicyLink: dynamic(() => import("@components/blog/SpicyLink")),
-    //GifDivider: dynamic(() => import("@components/blog/GifDivider")),
-    Head,
+interface Props {
+    source: MDXRemoteSerializeResult<Record<string, unknown>>;
+    frontMatter: PostFrontmatter;
 }
 
-interface PostPageProps {
-    source: MDXRemoteSerializeResult<Record<string, unknown>>
-    frontMatter: PostFrontmatter
-}
-
-export default function PostPage({ source, frontMatter }: PostPageProps) {
+export default function PostPage({ source, frontMatter }: Props) {
     return (
         <PostLayout>
-            <header>
-                <nav>
-                    <Link href="/">
-                        <a>👈 Go back home</a>
-                    </Link>
-                </nav>
-            </header>
-            <div className="post-header">
-                <h1>{frontMatter.title}</h1>
+            <div className="mb-8">
+                <h1 className="mb-0 text-red-500">{frontMatter.title}</h1>
             </div>
             <main>
                 <MDXRemote {...source} components={components} />
             </main>
-
-            <style jsx>{`
-                .post-header h1 {
-                    margin-bottom: 0;
-                }
-                .post-header {
-                    margin-bottom: 2rem;
-                }
-                .description {
-                    opacity: 0.6;
-                }
-            `}</style>
         </PostLayout>
-    )
+    );
 }
 
-export const getStaticProps = async ({ params }: { params: { slug: string } }) => {
-    const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`)
-    const source = fs.readFileSync(postFilePath)
+const defaultComponents = { SpicyLink, GifDivider };
+const components = {
+    ...defaultComponents,
+    Head,
+};
 
-    const { content, data } = matter(source)
+export const getStaticProps = async ({ params }: { params: { slug: string } }) => {
+    const postFilePath = path.join(POSTS_PATH, `${params.slug}.mdx`);
+    const source = fs.readFileSync(postFilePath);
+
+    const { content, data } = matter(source);
 
     const mdxSource = await serialize(content, {
-        // Optionally pass remark/rehype plugins
         mdxOptions: {
             remarkPlugins: [],
             rehypePlugins: [],
         },
         scope: data,
-    })
+    });
 
     return {
         props: {
             source: mdxSource,
             frontMatter: data as PostFrontmatter,
         },
-    }
-}
+    };
+};
 
 export const getStaticPaths = async () => {
-    const paths = postFilePaths
-        // Remove file extensions for page paths
-        .map((path) => path.replace(/\.mdx?$/, ""))
-        // Map the path into the static paths object required by Next.js
-        .map((slug) => ({ params: { slug } }))
+    const removeFileExtensionFromPath = (path: string) => path.replace(/\.mdx?$/, "");
+    const buildSlugPath = (slug: string) => ({ params: { slug } });
+    const paths = postFilePaths.map(removeFileExtensionFromPath).map(buildSlugPath);
 
     return {
         paths,
         fallback: false,
-    }
-}
+    };
+};
